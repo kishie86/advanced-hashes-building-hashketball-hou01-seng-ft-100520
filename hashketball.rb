f83aa9af521cafd5e3ef058477cc9ecbb16c0d22
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-25 23:00:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -64,8 +64,8 @@ def game_hash
     :away => {
     :team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
-    :players => [{
-      
+    :players => [
+      {
       :player_name => "Jeff Adrien",
       :number => "4",
       :shoe => "18",
